2f371152ca0bedd9ae28bc52a029b1ad04a8c158
Traci Armstrong <traciarms@gmail.com>
2017-01-26 17:36:58 -0800

    db changes

--- hireathena/settings.py
+++ hireathena/settings.py
@@ -75,11 +75,8 @@ WSGI_APPLICATION = 'hireathena.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'users': {
         'NAME': 'mydb',
         'ENGINE': 'django.db.backends.mysql',
         'USER': 'traciarms',
