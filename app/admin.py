4becf7dd3a894110d4ba569be3e4619d0adc28d3
Traci Armstrong <traciarms@gmail.com>
2017-01-26 18:24:50 -0800

    added models to admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.models import Team, User, Points
+
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date_created', 'date_updated')
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'team', 'date_create', 'date_updated')
+
+
+class PointsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'points', 'reason', 'date_created')
+
+
+admin.site.register(Team, TeamAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(Points, PointsAdmin)
--- app/models.py
+++ app/models.py
@@ -5,20 +5,20 @@ from django.db import models
 
 class Team(models.Model):
     name = models.CharField(max_length=250)
-    date_created = models.DateField()
-    date_updated = models.DateField()
+    date_created = models.DateField(auto_created=True)
+    date_updated = models.DateField(auto_now_add=True)
 
 
 class User(models.Model):
     name = models.CharField(max_length=250)
     team = models.ForeignKey(Team, null=True)
-    date_created = models.DateField()
-    date_updated = models.DateField()
+    date_created = models.DateField(auto_created=True)
+    date_updated = models.DateField(auto_now_add=True)
 
 
 class Points(models.Model):
     user = models.ForeignKey(User, null=True)
     points = models.IntegerField()
     reason = models.CharField(max_length=250)
-    date_created = models.DateField()
+    date_created = models.DateField(auto_created=True)
 
