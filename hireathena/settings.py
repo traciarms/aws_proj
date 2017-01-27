728e867929621a8b19b24e537e4d99985e1ae6ef
Traci Armstrong <traciarms@gmail.com>
2017-01-26 16:56:34 -0800

    modified allowed hosts

--- hireathena/settings.py
+++ hireathena/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-_rb#6m@-feuk_+#_#us1kd1td_x*%=rzai0u+y2mfyt&!(sg3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
