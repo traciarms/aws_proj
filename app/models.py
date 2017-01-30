b0ba871359e6ca67187f5327052bcc94c17ca9ce
Traci Armstrong <traciarms@gmail.com>
2017-01-30 09:57:02 -0800

    changed datetime to timezone

--- app/models.py
+++ app/models.py
@@ -1,14 +1,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from datetime import datetime
+from django.utils import timezone
 
 
 class Team(models.Model):
     """ The team model, contains name and date information """
     name = models.CharField(max_length=250)
-    date_created = models.DateTimeField(default=datetime.now, blank=True)
-    date_updated = models.DateTimeField(default=datetime.now, blank=True)
+    date_created = models.DateTimeField(default=timezone.now, blank=True)
+    date_updated = models.DateTimeField(default=timezone.now, blank=True)
 
     def __str__(self):
         return ("Name: {}, date created: {}, date updated: {}"
@@ -19,8 +19,8 @@ class AUser(models.Model):
     """ The User model, user name and the team they belong to """
     name = models.CharField(max_length=250)
     team = models.ForeignKey(Team, null=True)
-    date_created = models.DateTimeField(default=datetime.now, blank=True)
-    date_updated = models.DateTimeField(default=datetime.now, blank=True)
+    date_created = models.DateTimeField(default=timezone.now, blank=True)
+    date_updated = models.DateTimeField(default=timezone.now, blank=True)
 
     def __str__(self):
         return ("Name: {}, team: {}, date created: {}, date updated: {}"
@@ -34,7 +34,7 @@ class Point(models.Model):
     user = models.ForeignKey(AUser, null=True)
     points = models.IntegerField()
     reason = models.CharField(max_length=250)
-    date_created = models.DateTimeField(default=datetime.now, blank=True)
+    date_created = models.DateTimeField(default=timezone.now, blank=True)
 
     def __str__(self):
         return ("User: {}, points: {}, reason: {}, date created: {}"
